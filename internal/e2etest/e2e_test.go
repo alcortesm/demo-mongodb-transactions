b1498c06e2fccc71dc0b7919f066c2693c0261bf
Alberto Cortés <a.cortes@efg.gg>
2024-04-26 16:50:54 +0200

    test: add test to ensure AddUserToGroup respects Group consistency

--- internal/e2etest/e2e_test.go
+++ internal/e2etest/e2e_test.go
@@ -2,13 +2,18 @@ package e2etest
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/alcortesm/demo-mongodb-transactions/internal/application"
+	"github.com/alcortesm/demo-mongodb-transactions/internal/domain"
 	"github.com/alcortesm/demo-mongodb-transactions/internal/infra/mongo"
 	"github.com/alcortesm/demo-mongodb-transactions/internal/testhelp"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -89,4 +94,82 @@ func Test_AddOneUserToGroup(t *testing.T) {
 	modifiedGroup, err := fix.app.GetGroup(fix.ctx, groupID)
 	require.NoError(t, err)
 	require.Equal(t, []string{fix.ownerID, fix.userID}, modifiedGroup.Members())
+}
+
+// Test the app layer respects the Group invariants while adding users:
+//
+// Let's make many AddUserToGroup requests concurrently, more than the maximum
+// number of members allowed in a group.
+//
+// If the app layers respects the Group invariants, only MaxMembers-1 requests
+// will be successful, the rest will get a domain.ErrFullGroup error.
+//
+// For example, if MaxMembers is 5 and we sent 10 requests to add users:
+//   - 4 will be added correctly (5 minus the owner, which was already a member)
+//   - 6 will fail receive a domain.ErrFullGroup error
+func Test_Concurrency_AddLotsOfUsersConcurrentlyToGroup(t *testing.T) {
+	fix := struct {
+		*fixture
+		ownerID string
+	}{
+		fixture: newFixture(t),
+		ownerID: "some_owner_id",
+	}
+
+	// userID returns the id of a user based on the number n, for example, "user_id_0042"
+	userID := func(n int) string { return fmt.Sprintf("user_id_%02d", n) }
+
+	// GIVEN a group owned by fix.ownerID
+	groupID, err := fix.app.CreateGroup(fix.ctx, fix.ownerID)
+	require.NoError(t, err)
+
+	// GIVEN many users (2 * MaxMembers)
+	users := make([]string, 0, 2*domain.MaxMembers)
+	for i := range 2 * domain.MaxMembers {
+		users = append(users, userID(i))
+	}
+
+	// WHEN we add all the users to the group at the same time and keep track
+	// of how many requests got success vs how many requests got a
+	// domain.ErrGroupFull error.
+	var successCount, fullGroupErrorCount int
+	{
+		var wg sync.WaitGroup
+		wg.Add(len(users))
+
+		results := make([]error, len(users))
+		for i, id := range users {
+			go func() {
+				defer wg.Done()
+
+				// introduce an artificial delay in the app layer to improve the chance of
+				// processing all the requests at the same time
+				option := application.DelayBeforeUpdating(1 * time.Second)
+
+				results[i] = fix.app.AddUserToGroup(
+					fix.ctx,
+					id,
+					groupID,
+					option,
+				)
+			}()
+		}
+
+		wg.Wait()
+		for i, err := range results {
+			switch {
+			case err == nil:
+				successCount++
+			case errors.Is(err, domain.ErrGroupFull):
+				fullGroupErrorCount++
+			default:
+				t.Errorf("adding %s: %v", userID(i), err)
+			}
+		}
+	}
+
+	// THEN the number of requests that got a successful reponse is domain.MaxMembers-1
+	// and the number of requests that got an ErrGroupFull error is domain.MaxMembers+1
+	assert.Equal(t, domain.MaxMembers-1, successCount, "wrong successCount")
+	assert.Equal(t, domain.MaxMembers+1, fullGroupErrorCount, "wrong fullGroupErrorCount")
 }
